55fc6dfb86d1401ae308499d86c2ba83cdcce241
孤舟江海寄余生 <33691310+GuoKeMPF@users.noreply.github.com>
2022-01-19 22:13:05 +0800

    update file

---
+++ blog_site/mock/picture.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+
+import mockjs, { Random } from 'mockjs';
+
+const pictures: any[] = mockjs.mock({
+  'picture|50': [
+    {
+      'id|+1': 0,
+      src: Random.image('200x100', '#FF6600'),
+      create_time: '2021-12-10 11:54:29',
+      name: 'mock picture.png',
+    },
+  ],
+}).picture;
+
+export default {
+  'GET /api/picture': (req: Request, res: Response) => {
+    res.send({ data: pictures });
+  },
+
+  'POST /api/picture': (req: Request, res: Response) => {
+    const picture = { ...req.body, id: pictures.length };
+    pictures.unshift({
+      id: pictures.length,
+      src: Random.image('200x100', '#FF6600'),
+      create_time: '2021-12-10 11:54:29',
+      name: 'mock picture.png',
+    });
+    res.status(200).send(picture);
+  },
+
+  'DELETE /api/picture/:id': (req: Request, res: Response) => {
+    const { id } = req.params;
+    const index = pictures.findIndex((item) => item.id + '' === id + '');
+    if (index >= 0) {
+      pictures.splice(index, 1);
+      res.status(200).send(pictures);
+    } else {
+      res.status(404).send('Sorry, cant find that');
+    }
+  },
+};
