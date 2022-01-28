576d77aabf26ac7b6cde180ee596279976778782
mpf <319874759@qq.com>
2022-01-28 15:42:12 +0800

    loading

--- blog_site/src/components/Loading/index.tsx
+++ blog_site/src/components/Loading/index.tsx
@@ -4,39 +4,18 @@ import styles from './index.less';
 export default () => {
   return (
     <div className={styles.container}>
-      <div className={styles.gearbox}>
-        <div className={styles.overlay}></div>
-        <div className={`${styles.gear} ${styles.one}`}>
-          <div className={styles.gear_inner}>
-            <div className={styles.bar}></div>
-            <div className={styles.bar}></div>
-            <div className={styles.bar}></div>
-          </div>
-        </div>
-        <div className={`${styles.gear} ${styles.two}`}>
-          <div className={styles.gear_inner}>
-            <div className={styles.bar}></div>
-            <div className={styles.bar}></div>
-            <div className={styles.bar}></div>
-          </div>
-        </div>
-        <div className={`${styles.gear} ${styles.three}`}>
-          <div className={styles.gear_inner}>
-            <div className={styles.bar}></div>
-            <div className={styles.bar}></div>
-            <div className={styles.bar}></div>
-          </div>
-        </div>
-        <div className={`${styles.gear} ${styles.four} ${styles.large}`}>
-          <div className={styles.gear_inner}>
-            <div className={styles.bar}></div>
-            <div className={styles.bar}></div>
-            <div className={styles.bar}></div>
-            <div className={styles.bar}></div>
-            <div className={styles.bar}></div>
-            <div className={styles.bar}></div>
-          </div>
-        </div>
+      <div className={styles.nucleus}></div>
+      <div className={styles.track}>
+        <div className={styles.electronic}></div>
+      </div>
+      <div className={styles.track}>
+        <div className={styles.electronic}></div>
+      </div>
+      <div className={styles.track}>
+        <div className={styles.electronic}></div>
+      </div>
+      <div className={styles.track}>
+        <div className={styles.electronic}></div>
       </div>
     </div>
   );
