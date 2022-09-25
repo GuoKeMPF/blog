325898c0ef0c83a924f9db068ad7510595848850
Mpf Ubuntu <319874759@q.com>
2022-09-25 21:41:31 +0800

    allow get without jwt

--- blog_server/blog_server/settings.py
+++ blog_server/blog_server/settings.py
@@ -167,6 +167,10 @@ REST_FRAMEWORK = {
         "rest_framework.parsers.MultiPartParser",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
+        # 'rest_framework.permissions.IsAuthenticated',            # IsAuthenticated 仅通过认证的用户
+        # 'rest_framework.permissions.AllowAny',                   # AllowAny 允许所有用户
+        # 'rest_framework.permissions.IsAdminUser',                # IsAdminUser 仅管理员用户
+        # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',  # IsAuthenticatedOrReadOnly 认证的用户可以完全操作，否则只能get读取
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
