e7021f44191296cf42b085163bca5d420e1007d7
pf ma <319874759@qq.com>
2023-08-14 21:10:31 +0800

    chore(build web): 修改打包脚本

--- blog_site/build.sh
+++ blog_site/build.sh
@@ -5,11 +5,16 @@ eval "cd ./blog_site"
 echo "install first"
 eval "tyarn"
 
-
-
 echo "build site"
 eval "npm run build"
 
+# 检查打包结果
+if [ $? -eq 0 ]; then
+  echo "打包成功"
+  exit 0 # 打包成功，返回退出码0
+else
+  echo "打包失败"
+  exit 1 # 打包失败，返回非零退出码
+fi
 
 eval "cd ../"
-
