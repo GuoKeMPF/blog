c23a9573963cb12db96ed51a269224c05e29decd
孤舟江海寄余生 <319874759@qq.com>
2023-08-27 19:58:10 +0800

    test(server): 修改登陆测试用例

--- blog_server/e2e/test_login.py
+++ blog_server/e2e/test_login.py
@@ -3,27 +3,30 @@
 import requests
 import json
 from .config import account, password
-from .urls import getUrl 
+from .urls import getUrl
 from utils.cryptography.encrypt import encrypt
 import pytest
 from django.test import Client
 
 url = getUrl('login')
 encodePwd = encrypt(password)
+encodeUser = encrypt(account)
 param = {
-	"username": account,
-	"password": encodePwd,
+    "username": encodeUser,
+    "password": encodePwd,
 }
-j=json.dumps(param)
+j = json.dumps(param)
 print(j)
+
 
 @pytest.fixture
 def client():
-	return Client()
+    return Client()
+
 
 def test_my_function(client):
-	response = requests.post(url, data=json.dumps(param), headers={'Content-Type':'application/json'})
-	print("response")
-	print(response)
-	assert 200== 200
-
+    response = requests.post(url, data=json.dumps(param), headers={
+                             'Content-Type': 'application/json'})
+    print("response")
+    print(response)
+    assert response.status_code == 200
