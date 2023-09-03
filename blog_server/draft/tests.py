1ebf6ff783a76649fbc0a1052ae1ae5e9d75bd61
孤舟江海寄余生 <319874759@qq.com>
2023-09-03 21:51:23 +0800

    test(server): 草稿测试用例

--- blog_server/draft/tests.py
+++ blog_server/draft/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from django.urls import reverse
+
+from draft.models import Draft
+
+
+class DraftTest(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        Draft.objects.create(
+            title="test title 1", content="test content 1", author="test author 1", description="test description 1")
+        Draft.objects.create(
+            title="test title 2", content="test content 2", author="test author 2", description="test description 2")
+
+
+    def test_create_draft_without_login(self):
+        payload = {
+            "title":"test title 1", 
+            "content":"test content 1",
+        	"author":"test author 1",
+            "description":"test description 1"
+		}
+        response = self.client.post('/draft', payload)
+        self.assertEqual(response.status_code, 401)
+        
+
+    def test_draft_exists(self):
+        response = self.client.get('/draft')
+        self.assertEqual(response.status_code, 200)
