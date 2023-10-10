d726bc29fadec03c6ccbd8fb8b6214a9979b4cf6
GuZhouJiangHaiJiYuSheng <319874759@qq.com>
2023-10-10 18:10:35 +0800

    feat(site): umi site

--- umi_site/.umirc.ts
+++ umi_site/.umirc.ts
@@ -5,5 +5,23 @@ export default defineConfig({
     { path: "/", component: "index" },
     { path: "/docs", component: "docs" },
   ],
-  npmClient: 'yarn',
+
+  plugins: ["@umijs/plugins/dist/dva", "@umijs/plugins/dist/locale"],
+  locale: {
+    default: "zh-CN",
+    title: false,
+    // localStorage 中 umi_locale 值 > 浏览器检测 > default 设置的默认语言 > 中文
+    baseNavigator: true,
+    baseSeparator: "-",
+  },
+  ssr: {
+    builder: "esbuild",
+  },
+  dva: {
+    immer: {
+      enableES5: true,
+      enableAllPlugins: true,
+    },
+  },
+  npmClient: "yarn",
 });
