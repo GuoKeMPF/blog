9b71b1ecf08472b687b735b48816954c3d5dd670
GuZhouJiangHaiJiYuSheng <319874759@qq.com>
2023-10-23 18:16:30 +0800

    fix(site): error page

--- plugins/bg/src/pages/error/index.tsx
+++ plugins/bg/src/pages/error/index.tsx
@@ -1,18 +1,18 @@
 /** @format */
 
 const ErrorPage = () => {
-	return (
-		<div>
-			<p>
-				Space Invadors destroyed this page! Take revenge on them!
-				<br /> Use <span>Space</span> to shoot and <span>←</span>&#160;
-				<span>→</span> to move!&#160;&#160;&#160;
-				<button id='restart'>Restart</button>
-			</p>
+  return (
+    <div>
+      <p>
+        Space Invadors destroyed this page! Take revenge on them!
+        <br /> Use <span>Space</span> to shoot and <span>←</span>&#160;
+        <span>→</span> to move!&#160;&#160;&#160;
+        <button id="restart">Restart</button>
+      </p>
 
-			<canvas id='space-invaders' />
-		</div>
-	);
+      <canvas id="space-invaders" />
+    </div>
+  );
 };
 
 export default ErrorPage;
