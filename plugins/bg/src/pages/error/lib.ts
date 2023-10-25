5f747aab5e5858e21f36d63c9a0f854ac8575c09
GuZhouJiangHaiJiYuSheng <319874759@qq.com>
2023-10-25 18:51:31 +0800

    feat(site): update

--- plugins/bg/src/pages/error/lib.ts
+++ plugins/bg/src/pages/error/lib.ts
@@ -2,21 +2,26 @@
 
 import { ReactNode } from "react";
 
+const theme = {
+  backgroundColor: "#fff",
+  elementColor: "#000",
+};
+
 // General
-var screen, game;
+var screen: any, game: any;
 
 // Assets
-var invaderCanvas,
-	invaderMultiplier,
-	invaderSize = 20,
-	invaderAttackRate: number = 0,
-	invaderSpeed: number,
-	invaderSpawnDelay = 250;
+var invaderCanvas: any,
+  invaderMultiplier: any,
+  invaderSize = 20,
+  invaderAttackRate: number = 0,
+  invaderSpeed: number,
+  invaderSpawnDelay = 250;
 
 // Counter
 var i = 0,
-	kills = 0,
-	spawnDelayCounter = invaderSpawnDelay;
+  kills = 0,
+  spawnDelayCounter = invaderSpawnDelay;
 
 // Text
 var blocks = [
@@ -34,88 +39,85 @@ let gameSize = {
 };
 
 interface GamePropsType {
-	element: String | HTMLCanvasElement;
+  element: String | HTMLCanvasElement;
 }
 
 export class Game {
-	invaderShots: never[];
-	level: number;
-	lost: boolean;
-	player: any;
-	invaders: Invader[];
-	screen: CanvasRenderingContext2D;
-	invaderMultiplier: number = 1;
-	constructor({ element }: GamePropsType) {
-		this.level = -1;
-		this.lost = false;
-		this.invaders = [];
-		this.invaderShots = [];
-		let node: HTMLCanvasElement;
-		if (typeof element === "string") {
-			node = document.querySelector(element) as HTMLCanvasElement;
-		} else {
-			node = element as HTMLCanvasElement;
-		}
+  invaderShots: never[];
+  level: number;
+  lost: boolean;
+  player: any;
+  invaders: Invader[];
+  screen: CanvasRenderingContext2D;
+  invaderMultiplier: number = 1;
+  constructor({ element }: GamePropsType) {
+    this.level = -1;
+    this.lost = false;
+    this.invaders = [];
+    this.invaderShots = [];
+    let node: HTMLCanvasElement;
+    if (typeof element === "string") {
+      node = document.querySelector(element) as HTMLCanvasElement;
+    } else {
+      node = element as HTMLCanvasElement;
+    }
 
-		// Game Creation
-		const canvas = node;
-		if (!canvas) {
-			throw new Error("未找到画布元素");
-		}
-		const screen = canvas.getContext("2d") as CanvasRenderingContext2D;
-		this.screen = screen;
-		this.player = new Player({ screen });
-		var invaderAsset = new Image();
-		invaderAsset.onload = () => {
-			invaderCanvas = document.createElement("canvas");
-			invaderCanvas.width = invaderSize;
-			invaderCanvas.height = invaderSize;
-			invaderCanvas?.getContext("2d").drawImage(invaderAsset, 0, 0);
-			this.initGameStart();
-		};
-		invaderAsset.src = "//stillh.art/project/spaceInvaders/invader.gif";
-	}
-	initGameStart = () => {
-		let invaderMultiplier: number;
-		if (window.innerWidth > 1200) {
-			gameSize = {
-				width: 1200,
-				height: 500,
-			};
-			invaderMultiplier = 3;
-		} else if (window.innerWidth > 800) {
-			gameSize = {
-				width: 900,
-				height: 600,
-			};
-			invaderMultiplier = 2;
-		} else {
-			gameSize = {
-				width: 600,
-				height: 300,
-			};
-			invaderMultiplier = 1;
-		}
+    // Game Creation
+    const canvas = node;
+    if (!canvas) {
+      throw new Error("未找到画布元素");
+    }
+    const screen = canvas.getContext("2d") as CanvasRenderingContext2D;
+    this.screen = screen;
+    this.player = new Player({ screen });
+    const invaderAsset = new Image();
+    invaderAsset.onload = () => {
+      invaderCanvas = document.createElement("canvas");
+      invaderCanvas.width = invaderSize;
+      invaderCanvas.height = invaderSize;
+      invaderCanvas?.getContext("2d").drawImage(invaderAsset, 0, 0);
+      this.initGameStart();
+      this.draw();
+      // this.update();
+    };
+    invaderAsset.src = "//stillh.art/project/spaceInvaders/invader.gif";
+  }
+  initGameStart = () => {
+    let invaderMultiplier: number;
+    if (window.innerWidth > 1200) {
+      gameSize = {
+        width: 1200,
+        height: 500,
+      };
+      invaderMultiplier = 3;
+    } else if (window.innerWidth > 800) {
+      gameSize = {
+        width: 900,
+        height: 600,
+      };
+      invaderMultiplier = 2;
+    } else {
+      gameSize = {
+        width: 600,
+        height: 300,
+      };
+      invaderMultiplier = 1;
+    }
 
-		this.screen.canvas.width = gameSize.width;
-		this.screen.canvas.height = gameSize.height;
-		this.invaderMultiplier = invaderMultiplier;
-		kills = 0;
-		invaderAttackRate = 0.999;
-		invaderSpeed = 20;
-		spawnDelayCounter = invaderSpawnDelay;
+    this.screen.canvas.width = gameSize.width;
+    this.screen.canvas.height = gameSize.height;
+    this.invaderMultiplier = invaderMultiplier;
+    kills = 0;
+    invaderAttackRate = 0.999;
+    invaderSpeed = 20;
+    spawnDelayCounter = invaderSpawnDelay;
+  };
 
   update = () => {
     // Next level
     if (this.invaders.length === 0) {
       spawnDelayCounter += 1;
       if (spawnDelayCounter < invaderSpawnDelay) return;
-
-	update = () => {
-		// Next level
-		if (this.invaders.length === 0) {
-			spawnDelayCounter += 1;
-			if (spawnDelayCounter < invaderSpawnDelay) return;
 
       invaderAttackRate -= 0.002;
       invaderSpeed += 10;
@@ -149,72 +151,68 @@ export class Game {
 
     // Don't stop player & projectiles.. they look nice
     this.player.update();
-    for (i = 0; i < game.invaderShots.length; i++) {
+    for (i = 0; i < this.invaderShots.length; i++) {
       this.invaderShots[i].update();
     }
 
-		// Don't stop player & projectiles.. they look nice
-		this.player.update();
-		for (i = 0; i < this.invaderShots.length; i++) {
-			this.invaderShots[i].update();
-		}
+    // Don't stop player & projectiles.. they look nice
+    this.player.update();
+    for (i = 0; i < this.invaderShots.length; i++) {
+      this.invaderShots[i].update();
+    }
+  };
 
   draw = () => {
     if (this.lost) {
-      screen.fillStyle = "rgba(0, 0, 0, 0.03)";
-      screen.fillRect(0, 0, gameSize.width, gameSize.height);
+      this.screen.fillStyle = "rgba(0, 0, 0, 0.03)";
+      this.screen.fillRect(0, 0, gameSize.width, gameSize.height);
 
-	draw = () => {
-		if (this.lost) {
-			this.screen.fillStyle = "rgba(0, 0, 0, 0.03)";
-			this.screen.fillRect(0, 0, gameSize.width, gameSize.height);
+      this.screen.font = "55px Lucida Console";
+      this.screen.textAlign = "center";
+      this.screen.fillStyle = "white";
+      this.screen.fillText("You lost", gameSize.width / 2, gameSize.height / 2);
+      this.screen.fillText(
+        "Points: " + kills,
+        gameSize.width / 2,
+        gameSize.height / 2 + 30
+      );
+    } else {
+      this.screen.clearRect(0, 0, gameSize.width, gameSize.height);
 
-			this.screen.font = "55px Lucida Console";
-			this.screen.textAlign = "center";
-			this.screen.fillStyle = "white";
-			this.screen.fillText("You lost", gameSize.width / 2, gameSize.height / 2);
-			this.screen.fillText(
-				"Points: " + kills,
-				gameSize.width / 2,
-				gameSize.height / 2 + 30
-			);
-		} else {
-			this.screen.clearRect(0, 0, gameSize.width, gameSize.height);
+      this.screen.font = "10px Lucida Console";
+      this.screen.textAlign = "right";
+      this.screen.fillText(
+        "Points: " + kills,
+        gameSize.width,
+        gameSize.height - 12
+      );
+    }
 
-			this.screen.font = "10px Lucida Console";
-			this.screen.textAlign = "right";
-			this.screen.fillText(
-				"Points: " + kills,
-				gameSize.width,
-				gameSize.height - 12
-			);
-		}
+    this.screen.beginPath();
 
-		this.screen.beginPath();
+    this.player.draw();
+    if (!this.lost) {
+      for (let i = 0; i < this.invaders.length; i++) {
+        this.invaders[i].draw();
+      }
+    }
+    for (let i = 0; i < this.invaderShots.length; i++) {
+      this.invaderShots[i].draw();
+    }
 
-		this.player.draw();
-		if (!this.lost) {
-			for (let i = 0; i < this.invaders.length; i++) {
-				this.invaders[i].draw();
-			}
-		}
-		for (let i = 0; i < this.invaderShots.length; i++) {
-			this.invaderShots[i].draw();
-		}
+    this.screen.fill();
+  };
 
-		this.screen.fill();
-	};
-
-	invadersBelow = (invader) => {
-		return (
-			this.invaders.filter((b) => {
-				return (
-					Math.abs(invader.coordinates.x - b.coordinates.x) === 0 &&
-					b.coordinates.y > invader.coordinates.y
-				);
-			}).length > 0
-		);
-	};
+  invadersBelow = (invader) => {
+    return (
+      this.invaders.filter((b) => {
+        return (
+          Math.abs(invader.coordinates.x - b.coordinates.x) === 0 &&
+          b.coordinates.y > invader.coordinates.y
+        );
+      }).length > 0
+    );
+  };
 }
 
 interface InvaderType {
@@ -226,32 +224,32 @@ interface InvaderType {
 }
 
 class Invader implements InvaderType {
-	active: boolean;
-	coordinates: any;
-	size: { width: number; height: number };
-	patrolX: number;
-	speedX: any;
-	constructor({ coordinates, invaderSpeed }) {
-		this.active = true;
-		this.coordinates = coordinates;
-		this.size = {
-			width: invaderSize,
-			height: invaderSize,
-		};
+  active: boolean;
+  coordinates: any;
+  size: { width: number; height: number };
+  patrolX: number;
+  speedX: any;
+  constructor({ coordinates, invaderSpeed }) {
+    this.active = true;
+    this.coordinates = coordinates;
+    this.size = {
+      width: invaderSize,
+      height: invaderSize,
+    };
 
     this.patrolX = 0;
     this.speedX = invaderSpeed;
   }
 
-	draw = () => {
-		if (this.active)
-			screen.drawImage(invaderCanvas, this.coordinates.x, this.coordinates.y);
-	};
-	move = () => {
-		if (this.patrolX < 0 || this.patrolX > 100) {
-			this.speedX = -this.speedX;
-			this.patrolX += this.speedX;
-			this.coordinates.y += this.size.height;
+  draw = () => {
+    if (this.active)
+      screen.drawImage(invaderCanvas, this.coordinates.x, this.coordinates.y);
+  };
+  move = () => {
+    if (this.patrolX < 0 || this.patrolX > 100) {
+      this.speedX = -this.speedX;
+      this.patrolX += this.speedX;
+      this.coordinates.y += this.size.height;
 
       if (this.coordinates.y + this.size.height * 2 > gameSize.height)
         game.lost = true;
@@ -276,30 +274,29 @@ interface PlayerType {
 }
 
 class Player implements PlayerType {
-	active: boolean;
-	size: { width: number; height: number };
-	shooterHeat: number;
-	coordinates: { x: number; y: number };
-	projectile: never[];
-	keyboarder: any;
+  active: boolean;
+  size: { width: number; height: number };
+  shooterHeat: number;
+  coordinates: { x: number; y: number };
+  projectile: never[];
+  keyboarder: any;
 
-	screen: CanvasRenderingContext2D;
-	constructor({ screen }) {
-		this.active = true;
-		this.size = {
-			width: 16,
-			height: 8,
-		};
-		this.shooterHeat = -3;
-		this.coordinates = {
-			x: (gameSize.width / 2 - this.size.width / 2) | 0,
-			y: gameSize.height - this.size.height * 2,
-		};
-		this.screen = screen;
-		this.projectile = [];
-		this.keyboarder = new KeyController();
-	}
-
+  screen: CanvasRenderingContext2D;
+  constructor({ screen }) {
+    this.active = true;
+    this.size = {
+      width: 16,
+      height: 8,
+    };
+    this.shooterHeat = -3;
+    this.coordinates = {
+      x: (gameSize.width / 2 - this.size.width / 2) | 0,
+      y: gameSize.height - this.size.height * 2,
+    };
+    this.screen = screen;
+    this.projectile = [];
+    this.keyboarder = new KeyController();
+  }
   update = () => {
     for (var i = 0; i < this.projectile.length; i++)
       this.projectile[i].update();
@@ -341,19 +338,21 @@ class Player implements PlayerType {
     }
   };
 
-	draw = () => {
-		if (this.active) {
-			this.screen.rect(
-				this.coordinates.x,
-				this.coordinates.y,
-				this.size.width,
-				this.size.height
-			);
-			this.screen.rect(this.coordinates.x - 2, this.coordinates.y + 2, 20, 6);
-			this.screen.rect(this.coordinates.x + 6, this.coordinates.y - 4, 4, 4);
-		}
+  draw = () => {
+    if (this.active) {
+      this.screen.rect(
+        this.coordinates.x,
+        this.coordinates.y,
+        this.size.width,
+        this.size.height
+      );
+      this.screen.rect(this.coordinates.x - 2, this.coordinates.y + 2, 20, 6);
+      this.screen.rect(this.coordinates.x + 6, this.coordinates.y - 4, 4, 4);
+    }
 
-    for (var i = 0; i < this.projectile.length; i++) this.projectile[i].draw();
+    for (var i = 0; i < this.projectile.length; i++) {
+      this.projectile[i].draw();
+    }
   };
   destroy = () => {
     this.active = false;
