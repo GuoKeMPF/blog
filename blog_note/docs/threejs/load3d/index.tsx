1643e65472ae0bf114dfbade6c5f6490509a4380
panfeng <319874759@qq.com>
2024-01-03 18:08:38 +0800

    feat(note): load 3d

--- blog_note/docs/threejs/load3d/index.tsx
+++ blog_note/docs/threejs/load3d/index.tsx
@@ -2,83 +2,75 @@ import React, { useEffect, useRef } from 'react';
 
 import * as THREE from 'three';
 
-import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+
 
 interface ThreeObject {
-  camera: THREE.PerspectiveCamera;
-  scene: THREE.Scene;
-  renderer: THREE.WebGLRenderer;
+	camera: THREE.PerspectiveCamera;
+	scene: THREE.Scene;
+	renderer: THREE.WebGLRenderer;
 }
 
 function Load3D() {
-  const container = useRef<HTMLDivElement>(null);
-  const threeObject = useRef<ThreeObject>();
+	const container = useRef<HTMLDivElement>(null);
+	const threeObject = useRef<ThreeObject>();
 
-  const init = async () => {
-    const loader = new GLTFLoader();
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath('/examples/jsm/libs/draco/');
-    loader.setDRACOLoader(dracoLoader);
+	const init = async () => {
+		const loader = new GLTFLoader().setPath('/assets/threejs/glft/');
 
-    const { camera, scene, renderer } = threeObject.current;
-    loader.load(
-      // resource URL
-      '/assets/threejs/glft/Project.gltf',
-      // called when the resource is loaded
-      function (gltf) {
-        scene.add(gltf.scene);
+		const dracoLoader = new DRACOLoader();
+		dracoLoader.setDecoderPath('/assets/threejs/draco/javascript/');
+		loader.setDRACOLoader(dracoLoader);
+		const { camera, scene, renderer } = threeObject.current;
 
-        gltf.animations; // Array<THREE.AnimationClip>
-        gltf.scene; // THREE.Group
-        gltf.scenes; // Array<THREE.Group>
-        gltf.cameras; // Array<THREE.Camera>
-        gltf.asset; // Object
-      },
-      // called while loading is progressing
-      function (xhr) {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-      },
-      // called when loading has errors
-      function (error) {
-        console.log('An error happened');
-      },
-    );
-  };
+		const { width, height } = container.current.getBoundingClientRect();
+		renderer.setSize(width, height);
+		container.current.appendChild(renderer.domElement);
+		const gltf = await loader.loadAsync('Project.gltf')
 
-  useEffect(() => {
-    if (container.current) {
-      const camera = new THREE.PerspectiveCamera(
-        45,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        200,
-      );
-      const scene = new THREE.Scene();
+		scene.add(gltf.scene);
+		console.log(gltf);
 
-      const renderer = new THREE.WebGLRenderer();
 
-      threeObject.current = {
-        camera,
-        scene,
-        renderer,
-      };
-      init();
-      renderer.render(scene, camera);
-    }
-  }, []);
+		renderer.render(scene, camera);
+	};
 
-  return (
-    <div
-      ref={container}
-      style={{
-        width: '100%',
-        height: '100%',
-        minHeight: '400px',
-        minWidth: '400px',
-      }}
-    ></div>
-  );
+	useEffect(() => {
+		if (container.current) {
+			const { width, height } = container.current.getBoundingClientRect();
+			const camera = new THREE.PerspectiveCamera(
+				45,
+				width / height,
+				0.1,
+				200,
+			);
+			const scene = new THREE.Scene();
+			const renderer = new THREE.WebGLRenderer();
+			threeObject.current = {
+				camera,
+				scene,
+				renderer,
+			};
+			init();
+
+		}
+	}, []);
+
+	return (
+		<div
+			ref={container}
+			style={{
+				width: '100%',
+				height: '100%',
+				minHeight: '600px',
+				minWidth: '400px',
+			}}
+		></div>
+	);
 }
 
 export default Load3D;
--- blog_note/docs/threejs/text/CSS2d.tsx
+++ blog_note/docs/threejs/text/CSS2d.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useMemo, useRef } from 'react';
 
 import {
-  CSS2DObject,
-  CSS2DRenderer,
+	CSS2DObject,
+	CSS2DRenderer,
 } from 'three/addons/renderers/CSS2DRenderer.js';
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
@@ -18,233 +18,235 @@ import moon_1024 from './textures/moon_1024.jpg';
 import useStyles from './CSS2d.styles';
 
 interface ThreeObject {
-  camera: THREE.PerspectiveCamera;
-  scene: THREE.Scene;
-  renderer: THREE.WebGLRenderer;
-  css2DRenderer: CSS2DRenderer;
-  gui: GUI;
-  clock: THREE.Clock;
-  moon?: THREE.Mesh;
+	camera: THREE.PerspectiveCamera;
+	scene: THREE.Scene;
+	renderer: THREE.WebGLRenderer;
+	css2DRenderer: CSS2DRenderer;
+	gui: GUI;
+	clock: THREE.Clock;
+	moon?: THREE.Mesh;
 }
 
 function CSS2d() {
-  const container = useRef<HTMLDivElement>(null);
-  const guiRef = useRef<HTMLDivElement>(null);
+	const container = useRef<HTMLDivElement>(null);
+	const guiRef = useRef<HTMLDivElement>(null);
 
-  const threeObject = useRef<ThreeObject>();
-  const requestAnimationFrameId = useRef<number>();
+	const threeObject = useRef<ThreeObject>();
+	const requestAnimationFrameId = useRef<number>();
 
-  const EARTH_RADIUS = useMemo(() => 1, []);
-  const MOON_RADIUS = useMemo(() => 0.27, []);
+	const EARTH_RADIUS = useMemo(() => 1, []);
+	const MOON_RADIUS = useMemo(() => 0.27, []);
 
-  const { styles } = useStyles();
+	const { styles } = useStyles();
 
-  function initGui() {
-    const { gui, camera } = threeObject.current;
-    const layers = {
-      'Toggle Name': function () {
-        camera.layers.toggle(0);
-      },
-      'Toggle Mass': function () {
-        camera.layers.toggle(1);
-      },
-      'Enable All': function () {
-        camera.layers.enableAll();
-      },
-      'Disable All': function () {
-        camera.layers.disableAll();
-      },
-    };
-    gui.title('Camera Layers');
-    gui.add(layers, 'Toggle Name');
-    gui.add(layers, 'Toggle Mass');
-    gui.add(layers, 'Enable All');
-    gui.add(layers, 'Disable All');
-    gui.open();
-  }
+	function initGui() {
+		const { gui, camera } = threeObject.current;
+		const layers = {
+			'Toggle Name': function () {
+				camera.layers.toggle(0);
+			},
+			'Toggle Mass': function () {
+				camera.layers.toggle(1);
+			},
+			'Enable All': function () {
+				camera.layers.enableAll();
+			},
+			'Disable All': function () {
+				camera.layers.disableAll();
+			},
+		};
+		gui.title('Camera Layers');
+		gui.add(layers, 'Toggle Name');
+		gui.add(layers, 'Toggle Mass');
+		gui.add(layers, 'Enable All');
+		gui.add(layers, 'Disable All');
+		gui.open();
+	}
 
-  function init() {
-    if (threeObject.current) {
-      const { camera, renderer, scene, css2DRenderer } = threeObject.current;
-      const { width, height } = container.current.getBoundingClientRect();
+	function init() {
+		if (threeObject.current) {
+			const { camera, renderer, scene, css2DRenderer } = threeObject.current;
+			const { width, height } = container.current.getBoundingClientRect();
 
-      camera.position.set(10, 5, 20);
-      camera.layers.enableAll();
+			camera.position.set(10, 5, 20);
+			camera.layers.enableAll();
 
-      const dirLight = new THREE.DirectionalLight(0xffffff, 3);
-      dirLight.position.set(0, 0, 1);
-      dirLight.layers.enableAll();
-      scene.add(dirLight);
+			const dirLight = new THREE.DirectionalLight(0xffffff, 3);
+			dirLight.position.set(0, 0, 1);
+			dirLight.layers.enableAll();
+			scene.add(dirLight);
 
-      const axesHelper = new THREE.AxesHelper(5);
-      axesHelper.layers.enableAll();
-      scene.add(axesHelper);
+			const axesHelper = new THREE.AxesHelper(5);
+			axesHelper.layers.enableAll();
+			scene.add(axesHelper);
 
-      const textureLoader = new THREE.TextureLoader();
-      const earthGeometry = new THREE.SphereGeometry(EARTH_RADIUS, 16, 16);
-      const earthMaterial = new THREE.MeshPhongMaterial({
-        specular: 0x333333,
-        shininess: 5,
-        map: textureLoader.load(earth_atmos_2048),
-        specularMap: textureLoader.load(earth_specular_2048),
-        normalMap: textureLoader.load(earth_normal_2048),
-        normalScale: new THREE.Vector2(0.85, 0.85),
-      });
-      earthMaterial.map.colorSpace = THREE.SRGBColorSpace;
-      const earth = new THREE.Mesh(earthGeometry, earthMaterial);
-      scene.add(earth);
+			const textureLoader = new THREE.TextureLoader();
+			const earthGeometry = new THREE.SphereGeometry(EARTH_RADIUS, 16, 16);
+			const earthMaterial = new THREE.MeshPhongMaterial({
+				specular: 0x333333,
+				shininess: 5,
+				map: textureLoader.load(earth_atmos_2048),
+				specularMap: textureLoader.load(earth_specular_2048),
+				normalMap: textureLoader.load(earth_normal_2048),
+				normalScale: new THREE.Vector2(0.85, 0.85),
+			});
+			earthMaterial.map.colorSpace = THREE.SRGBColorSpace;
+			const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+			scene.add(earth);
 
-      const moonGeometry = new THREE.SphereGeometry(MOON_RADIUS, 16, 16);
-      const moonMaterial = new THREE.MeshPhongMaterial({
-        shininess: 5,
-        map: textureLoader.load(moon_1024),
-      });
-      moonMaterial.map.colorSpace = THREE.SRGBColorSpace;
-      const moon = new THREE.Mesh(moonGeometry, moonMaterial);
-      scene.add(moon);
+			const moonGeometry = new THREE.SphereGeometry(MOON_RADIUS, 16, 16);
+			const moonMaterial = new THREE.MeshPhongMaterial({
+				shininess: 5,
+				map: textureLoader.load(moon_1024),
+			});
+			moonMaterial.map.colorSpace = THREE.SRGBColorSpace;
+			const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+			scene.add(moon);
 
-      earth.layers.enableAll();
-      moon.layers.enableAll();
+			earth.layers.enableAll();
+			moon.layers.enableAll();
 
-      const earthDiv = document.createElement('div');
-      earthDiv.className = styles.label;
-      earthDiv.textContent = 'Earth';
-      earthDiv.style.backgroundColor = 'transparent';
+			const earthDiv = document.createElement('div');
+			earthDiv.className = styles.label;
+			earthDiv.textContent = 'Earth';
+			earthDiv.style.backgroundColor = 'transparent';
 
-      const earthLabel = new CSS2DObject(earthDiv);
-      earthLabel.position.set(1.5 * EARTH_RADIUS, 0, 0);
-      earthLabel.center.set(0, 1);
-      earth.add(earthLabel);
-      earthLabel.layers.set(0);
+			const earthLabel = new CSS2DObject(earthDiv);
+			earthLabel.position.set(1.5 * EARTH_RADIUS, 0, 0);
+			earthLabel.center.set(0, 1);
+			earth.add(earthLabel);
+			earthLabel.layers.set(0);
 
-      const earthMassDiv = document.createElement('div');
-      earthMassDiv.className = styles.label;
-      earthMassDiv.textContent = '5.97237e24 kg';
-      earthMassDiv.style.backgroundColor = 'transparent';
+			const earthMassDiv = document.createElement('div');
+			earthMassDiv.className = styles.label;
+			earthMassDiv.textContent = '5.97237e24 kg';
+			earthMassDiv.style.backgroundColor = 'transparent';
 
-      const earthMassLabel = new CSS2DObject(earthMassDiv);
-      earthMassLabel.position.set(1.5 * EARTH_RADIUS, 0, 0);
-      earthMassLabel.center.set(0, 0);
-      earth.add(earthMassLabel);
-      earthMassLabel.layers.set(1);
+			const earthMassLabel = new CSS2DObject(earthMassDiv);
+			earthMassLabel.position.set(1.5 * EARTH_RADIUS, 0, 0);
+			earthMassLabel.center.set(0, 0);
+			earth.add(earthMassLabel);
+			earthMassLabel.layers.set(1);
 
-      const moonDiv = document.createElement('div');
-      moonDiv.className = styles.label;
-      moonDiv.textContent = 'Moon';
-      moonDiv.style.backgroundColor = 'transparent';
+			const moonDiv = document.createElement('div');
+			moonDiv.className = styles.label;
+			moonDiv.textContent = 'Moon';
+			moonDiv.style.backgroundColor = 'transparent';
 
-      const moonLabel = new CSS2DObject(moonDiv);
-      moonLabel.position.set(1.5 * MOON_RADIUS, 0, 0);
-      moonLabel.center.set(0, 1);
-      moon.add(moonLabel);
-      moonLabel.layers.set(0);
+			const moonLabel = new CSS2DObject(moonDiv);
+			moonLabel.position.set(1.5 * MOON_RADIUS, 0, 0);
+			moonLabel.center.set(0, 1);
+			moon.add(moonLabel);
+			moonLabel.layers.set(0);
 
-      const moonMassDiv = document.createElement('div');
-      moonMassDiv.className = styles.label;
-      moonMassDiv.textContent = '7.342e22 kg';
-      moonMassDiv.style.backgroundColor = 'transparent';
+			const moonMassDiv = document.createElement('div');
+			moonMassDiv.className = styles.label;
+			moonMassDiv.textContent = '7.342e22 kg';
+			moonMassDiv.style.backgroundColor = 'transparent';
 
-      const moonMassLabel = new CSS2DObject(moonMassDiv);
-      moonMassLabel.position.set(1.5 * MOON_RADIUS, 0, 0);
-      moonMassLabel.center.set(0, 0);
-      moon.add(moonMassLabel);
-      moonMassLabel.layers.set(1);
+			const moonMassLabel = new CSS2DObject(moonMassDiv);
+			moonMassLabel.position.set(1.5 * MOON_RADIUS, 0, 0);
+			moonMassLabel.center.set(0, 0);
+			moon.add(moonMassLabel);
+			moonMassLabel.layers.set(1);
 
-      renderer.setPixelRatio(window.devicePixelRatio);
-      renderer.setSize(width, height);
-      container.current.appendChild(renderer.domElement);
+			renderer.setPixelRatio(window.devicePixelRatio);
+			renderer.setSize(width, height);
+			container.current.appendChild(renderer.domElement);
 
-      css2DRenderer.setSize(width, height);
-      css2DRenderer.domElement.style.position = 'absolute';
-      css2DRenderer.domElement.style.top = '0px';
-      container.current.appendChild(css2DRenderer.domElement);
+			css2DRenderer.setSize(width, height);
+			css2DRenderer.domElement.style.position = 'absolute';
+			css2DRenderer.domElement.style.top = '0px';
+			container.current.appendChild(css2DRenderer.domElement);
 
-      const controls = new OrbitControls(camera, css2DRenderer.domElement);
-      controls.minDistance = 5;
-      controls.maxDistance = 100;
+			const controls = new OrbitControls(camera, css2DRenderer.domElement);
+			controls.minDistance = 5;
+			controls.maxDistance = 100;
 
-      threeObject.current.moon = moon;
-    }
-  }
+			threeObject.current.moon = moon;
+		}
+	}
 
-  function onWindowResize() {
-    if (threeObject.current) {
-      const { camera, renderer, css2DRenderer } = threeObject.current;
-      const { width, height } = container.current.getBoundingClientRect();
-      camera.aspect = width / height;
-      camera.updateProjectionMatrix();
-      renderer.setSize(width, height);
-      css2DRenderer.setSize(width, height);
-    }
-  }
+	function onWindowResize() {
+		if (threeObject.current) {
+			const { camera, renderer, css2DRenderer } = threeObject.current;
+			const { width, height } = container.current.getBoundingClientRect();
+			camera.aspect = width / height;
+			camera.updateProjectionMatrix();
+			renderer.setSize(width, height);
+			css2DRenderer.setSize(width, height);
+		}
+	}
 
-  function animate() {
-    if (threeObject.current) {
-      const { camera, renderer, css2DRenderer, clock, scene, moon } =
-        threeObject.current;
-      requestAnimationFrameId.current = requestAnimationFrame(animate);
-      const elapsed = clock.getElapsedTime();
-      moon?.position.set(Math.sin(elapsed) * 5, 0, Math.cos(elapsed) * 5);
-      renderer.render(scene, camera);
-      css2DRenderer.render(scene, camera);
-    }
-  }
+	function animate() {
+		if (threeObject.current) {
+			const { camera, renderer, css2DRenderer, clock, scene, moon } =
+				threeObject.current;
+			requestAnimationFrameId.current = requestAnimationFrame(animate);
+			const elapsed = clock.getElapsedTime();
+			moon?.position.set(Math.sin(elapsed) * 5, 0, Math.cos(elapsed) * 5);
+			renderer.render(scene, camera);
+			css2DRenderer.render(scene, camera);
+		}
+	}
 
-  useEffect(() => {
-    if (container.current) {
-      const gui = new GUI({ container: guiRef.current, injectStyles: true });
-      const clock = new THREE.Clock();
-      const camera = new THREE.PerspectiveCamera(
-        45,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        200,
-      );
-      const scene = new THREE.Scene();
+	useEffect(() => {
+		if (container.current) {
+			const gui = new GUI({ container: guiRef.current, injectStyles: true });
+			const clock = new THREE.Clock();
 
-      const renderer = new THREE.WebGLRenderer();
-      const css2DRenderer = new CSS2DRenderer();
+			const { width, height } = container.current.getBoundingClientRect();
+			const camera = new THREE.PerspectiveCamera(
+				45,
+				width / height,
+				0.1,
+				200,
+			);
+			const scene = new THREE.Scene();
 
-      threeObject.current = {
-        gui,
-        clock,
-        camera,
-        scene,
-        renderer,
-        css2DRenderer,
-      };
-      init();
-      initGui();
-      renderer.render(scene, camera);
+			const renderer = new THREE.WebGLRenderer();
+			const css2DRenderer = new CSS2DRenderer();
 
-      animate();
+			threeObject.current = {
+				gui,
+				clock,
+				camera,
+				scene,
+				renderer,
+				css2DRenderer,
+			};
+			init();
+			initGui();
+			renderer.render(scene, camera);
 
-      window.addEventListener('resize', onWindowResize);
-    }
-    return () => {
-      cancelAnimationFrame(requestAnimationFrameId.current);
-      window.removeEventListener('resize', onWindowResize);
-    };
-  }, []);
+			animate();
 
-  return (
-    <div
-      style={{
-        position: 'relative',
-      }}
-    >
-      <div
-        ref={container}
-        style={{
-          width: '100%',
-          height: '100%',
-          minHeight: '400px',
-          minWidth: '400px',
-        }}
-      ></div>
-      <div ref={guiRef} style={{ position: 'absolute', top: '0px' }}></div>
-    </div>
-  );
+			window.addEventListener('resize', onWindowResize);
+		}
+		return () => {
+			cancelAnimationFrame(requestAnimationFrameId.current);
+			window.removeEventListener('resize', onWindowResize);
+		};
+	}, []);
+
+	return (
+		<div
+			style={{
+				position: 'relative',
+			}}
+		>
+			<div
+				ref={container}
+				style={{
+					width: '100%',
+					height: '100%',
+					minHeight: '400px',
+					minWidth: '400px',
+				}}
+			></div>
+			<div ref={guiRef} style={{ position: 'absolute', top: '0px' }}></div>
+		</div>
+	);
 }
 
 export default CSS2d;
