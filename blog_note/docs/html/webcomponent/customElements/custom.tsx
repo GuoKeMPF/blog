906d1cdb14ea6de1fb1869e6f1c20313819dd7e0
孤舟江海寄余生 <319874759@qq.com>
2024-01-09 21:05:15 +0800

    fix(note): 自定义元素重复定义处理

--- blog_note/docs/html/webcomponent/customElements/custom.tsx
+++ blog_note/docs/html/webcomponent/customElements/custom.tsx
@@ -1,20 +1,20 @@
-import React, { useEffect, Fragment, type FC } from "react";
-import { PopupInfo } from "./Elements";
+import React, { Fragment, useEffect, type FC } from 'react';
+import { PopupInfo } from './Elements';
 
 import image from './image.jpeg';
 
 export const CustomizedBuiltInElement: FC = () => {
-
   useEffect(() => {
-    customElements.define("popup-info", PopupInfo);
-  }, [])
+    if (!customElements.get('popup-info')) {
+      customElements.define('popup-info', PopupInfo);
+    }
+  }, []);
 
-  return <Fragment>
-    <popup-info
-      img={image}
-      data-text="测试描述文字"></popup-info>
-  </Fragment >;
+  return (
+    <Fragment>
+      <popup-info img={image} data-text="测试描述文字"></popup-info>
+    </Fragment>
+  );
 };
 
-export default CustomizedBuiltInElement
-
+export default CustomizedBuiltInElement;
